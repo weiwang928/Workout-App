79109a74f44203a9f65fae91e184cd6d6297613a
Wei Wang <weiwang701257@gmail.com>
2020-03-28 08:59:47 -0700

    initial commit

---
+++ WorkoutApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorkoutApp
+//
+//  Created by user921391 on 3/28/20.
+//  Copyright © 2020 Wei Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
