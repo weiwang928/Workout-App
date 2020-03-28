fa1e20acfd7ee2e4819ac843b7442e029c6a032f
Wei Wang <weiwang701257@gmail.com>
2020-03-28 16:46:34 -0700

    Tried several approaches to load scenes to the simulator. None worked

--- WorkoutApp/HomePageController.swift
+++ WorkoutApp/HomePageController.swift
@@ -10,6 +10,7 @@ import Foundation
 import UIKit
 
 class HomePageController: UIViewController {
+    @IBOutlet var mainView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- WorkoutApp/SceneDelegate.swift
+++ WorkoutApp/SceneDelegate.swift
@@ -27,7 +27,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-           // window.rootViewController = UIHostingController(rootView: homePageController)
+           // window.rootViewController = UIHostingController(rootView: contentView)
             window.rootViewController = homePageController
             self.window = window
             window.makeKeyAndVisible()
