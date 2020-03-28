66b23030bd00537431037ad7419e246d0bbfe632
Wei Wang <weiwang701257@gmail.com>
2020-03-28 15:23:38 -0700

    Change custom class for scenes in main storyboard

---
+++ WorkoutApp/HomePageController.swift
@@ -0,0 +1,17 @@
+//
+//  HomePageController.swift
+//  WorkoutApp
+//
+//  Created by user921391 on 3/28/20.
+//  Copyright © 2020 Wei Wang. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class HomePageController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
